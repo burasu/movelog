2d068d9377038601a26415e52471e2ea49aab0f3
Blas Fernández Segura <burasu@gmail.com>
2015-09-17 11:56:03 +0200

    Iniciamos la estructura del proyecto.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ejemplo de tabla de ejercicios desarrollado bajo AngularJS.
---
+++ app/scripts/controllers.js
@@ -0,0 +1,15 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('movelog.controllers', []);
+
+    function MainController ($scope)
+    {
+    }
+
+    angular
+        .module('movelog.controllers')
+        .controller('MainController', MainController);
+
+})();
